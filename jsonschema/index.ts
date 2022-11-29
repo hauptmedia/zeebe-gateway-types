32c7a09d4f21c23cfcb0727e6020559d6ff6ced5
Julian Haupt <julian@haupt.media>
2022-11-29 20:26:10 +0100

    Updates

---
+++ jsonschema/index.ts
@@ -0,0 +1,31 @@
+import * as ActivateJobs from './io/zeebe/command/v1/ActivateJobs.json'
+import * as CancelProcessInstance from './io/zeebe/command/v1/CancelProcessInstance.json'
+import * as CompleteJob from './io/zeebe/command/v1/CompleteJob.json'
+import * as CreateProcessInstance from './io/zeebe/command/v1/CreateProcessInstance.json'
+import * as CreateProcessInstanceWithResult from './io/zeebe/command/v1/CreateProcessInstanceWithResult.json'
+import * as DeployResource from './io/zeebe/command/v1/DeployResource.json'
+import * as FailJob from './io/zeebe/command/v1/FailJob.json'
+import * as PublishMessage from './io/zeebe/command/v1/PublishMessage.json'
+import * as ResolveIncident from './io/zeebe/command/v1/ResolveIncident.json'
+import * as ThrowError from './io/zeebe/command/v1/ThrowError.json'
+import * as Topology from './io/zeebe/command/v1/Topology.json'
+
+export const ZeebeGatewayCommandJsonSchemaRegistry = {
+    'io.zeebe.command.v1.ActivateJobs': ActivateJobs,
+    'io.zeebe.command.v1.CancelProcessInstance': CancelProcessInstance,
+    'io.zeebe.command.v1.CompleteJob': CompleteJob,
+    'io.zeebe.command.v1.CreateProcessInstance': CreateProcessInstance,
+    'io.zeebe.command.v1.CreateProcessInstanceWithResult': CreateProcessInstanceWithResult,
+    'io.zeebe.command.v1.DeployResource': DeployResource,
+    'io.zeebe.command.v1.FailJob': FailJob,
+    'io.zeebe.command.v1.PublishMessage': PublishMessage,
+    'io.zeebe.command.v1.ResolveIncident': ResolveIncident,
+    'io.zeebe.command.v1.ThrowError': ThrowError,
+    'io.zeebe.command.v1.Topology': Topology
+}
+
+export type ZeebeGatewayCommandTypes =
+    'io.zeebe.command.v1.ActivateJobs' | 'io.zeebe.command.v1.CancelProcessInstance' | 'io.zeebe.command.v1.CompleteJob' |
+    'io.zeebe.command.v1.CreateProcessInstance' | 'io.zeebe.command.v1.CreateProcessInstanceWithResult' | 'io.zeebe.command.v1.DeployResource' |
+    'io.zeebe.command.v1.FailJob' | 'io.zeebe.command.v1.PublishMessage' | 'io.zeebe.command.v1.ResolveIncident' |
+    'io.zeebe.command.v1.ThrowError' | 'io.zeebe.command.v1.Topology';
